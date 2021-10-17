5c67f2764055b21f861c4a7a91a6940a3cf05d95
khalid <kaled1418g@gmai.com>
2021-10-17 23:49:52 +0300

    exception

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,13 @@
 fun main() {
-    var x:String? = "null"
-    var title =x?:"new title"
-    println(title)
+    var num1 = readLine()!!.toInt()
+    var num2 = readLine()!!.toInt()
+divide(num1, num2)
+}
+fun divide(num1:Int , num2:Int){
+    try {
+        println("$num1 / $num2 =  ${num1 / num2}")
+    }catch (exception : ArithmeticException){
+        println("can't divide by zero")
+    }
+
 }
